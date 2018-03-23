9cd46d009f421ea4eedd44d3ff4eee270f4f7161
JanMod <jan-moder@hotmail.de>
2018-03-23 19:59:16 +0100

    install angular material

--- spotifyGuessGame/src/app/app.module.ts
+++ spotifyGuessGame/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { RoomsComponent } from './components/rooms/rooms.component';
+import { CreateRoomComponent } from './components/create-room/create-room.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoomsComponent,
+    CreateRoomComponent
   ],
   imports: [
     BrowserModule
