c52328a829ee74096f960325f45a97beb4889b58
JanMod <jan-moder@hotmail.de>
2018-03-31 19:43:27 +0200

    create ws object

---
+++ spotifyGuessGame/server/ws.js
@@ -0,0 +1,17 @@
+var socket = require('socket.io');
+
+module.exports = function (app) {
+    var _io = global.io = socket(app)
+
+    _io.sockets.on('connection', socket => {
+        console.log(socket.id);
+        socket.join('newRoom');
+        socket.emit('newRoom', { nothing: 'lul' });
+    })
+
+    _io.broadcastNewRoom =  function (room) {
+        _io.sockets.to('newRoom').emit('newRoom', room);
+    }
+
+
+}
