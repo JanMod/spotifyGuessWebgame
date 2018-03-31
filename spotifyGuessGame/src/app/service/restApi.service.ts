3da44342245266102940f660993396c3a8d7dd1c
JanMod <jan-moder@hotmail.de>
2018-03-31 19:41:31 +0200

    update restApi to communicate with the server

--- spotifyGuessGame/src/app/service/restApi.service.ts
+++ spotifyGuessGame/src/app/service/restApi.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Component } from '@angular/core/src/metadata/directives';
-
+import * as Rx from 'rxjs/Rx';
+import * as io from 'socket.io-client';
 
 @Injectable()
 
@@ -9,18 +10,70 @@ export class RestApiService {
 
     private createRoomRoute: string;
     response: any;
+    private socket;
     constructor(private http: HttpClient) {
+    }
 
+
+    private subject: Rx.Subject<MessageEvent>;
+
+    public connect(url): Rx.Subject<MessageEvent> {
+        if (!this.subject) {
+            this.subject = this.create(url);
+            console.log("Successfully connected: " + url);
+        }
+        return this.subject;
+    }
+
+    public connectIO (url): Rx.Observable<MessageEvent>{
+        this.socket = io.connect(url);
+        var _socket = this.socket;
+        return Rx.Observable.fromEventPattern( data =>{
+            _socket.on('newRoom', data);
+        }
+            
+        )
+      
+    }
+
+
+
+    private create(url): Rx.Subject<MessageEvent> {
+        let ws = new WebSocket(url);
+
+        let observable = Rx.Observable.create(
+            (obs: Rx.Observer<MessageEvent>) => {
+                ws.onmessage = obs.next.bind(obs);
+                ws.onerror = obs.error.bind(obs);
+                ws.onclose = obs.complete.bind(obs);
+                return ws.close.bind(ws);
+            })
+        let observer = {
+            next: (data: Object) => {
+                if (ws.readyState === WebSocket.OPEN) {
+                    ws.send(JSON.stringify(data));
+                }
+            }
+        }
+        return Rx.Subject.create(observer, observable);
     }
 
     ngOnInit() {
-        this.createRoomRoute = 'http://localhost:8000/api/createRoom';
+        this.createRoomRoute = 'api/createRoom';
     }
 
     createRoom(data) {
+        return this.http.post('http://localhost:8000/api/createRoom', data);
+    }
 
-        return this.http.post('api/createRoom', data);
+    getRooms(){
+        return this.http.get('http://localhost:8000/api/Rooms');
+    }
+
+    leaveRoom() {
 
     }
+
 }
 
+// 
