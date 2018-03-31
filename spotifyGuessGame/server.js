4e324b81ebd9b0f17564432c45d34f68e7d5b3e2
JanMod <jan-moder@hotmail.de>
2018-03-31 19:43:09 +0200

    add CORS for local testing

--- spotifyGuessGame/server.js
+++ spotifyGuessGame/server.js
@@ -3,21 +3,32 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const http = require('http');
 const app = express();
-var expressWs = require('express-ws')(app);
-// API file for interacting with MongoDB
 const api = require('./server/routes/api');
 
+// API file for interacting with MongoDB
 
-app.ws('/echo', function(ws, req) {
-    ws.on('message', function(msg) {
-      ws.send(msg);
-    });
-  });
-  
-  
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 // Parsers
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Angular DIST output folder
 app.use(express.static(path.join(__dirname, 'dist')));
@@ -37,3 +48,6 @@ app.set('port', port);
 const server = http.createServer(app);
 
 server.listen(port, () => console.log(`Running on localhost:${port}`));
+
+const ws = require('./server/ws')(server);
+
